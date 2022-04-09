aaa0efa2f8c12740a15e8e225d6c59e4ae74ef05
EngeReyes23 <engelreyes23@gmail.com>
2022-04-09 17:49:00 -0600

    valores random al hacer click

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,8 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
+// import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +17,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+        // Toast.makeText(this, "button clicked",
+        //   Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = (1..6).random()
+
+        resultText.text = randomInt.toString()
     }
 }
